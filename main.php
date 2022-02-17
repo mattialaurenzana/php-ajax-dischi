46a1513d66b5132ec52a1f60d0279cd8374491b4
mattia laurenzana <mattilaure@gmail.com>
2022-02-17 18:22:31 +0100

    Correzione ciclo foreach, snellimento codice

--- main.php
+++ main.php
@@ -13,7 +13,7 @@ include './database/db.php';
     <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    <title>Document</title>
+    <title>spotify dischi</title>
 </head>
 <body>
     
@@ -23,32 +23,19 @@ include './database/db.php';
         </header>
         <div class="container">
             <div class="row">
-                
-                    <!-- <img :src="" alt="music disk">
-                    <div class="title"></div>
-                    <div class="author"></div>
-                    <span class="year"></span> -->
 
                     <?php
                         foreach($db as $ndisk => $disk){
-                            foreach($disk as $key => $value){
-                              if($key === 'poster'){
-                                $poster = $value;
-                              }else if($key === 'title'){
-                                  $title = $value;
-                              }else if($key === 'author'){
-                                  $author = $value;
-                              }else if($key === 'year'){
-                                  $year = $value;
-                              }
-                            }
-                            echo "<div class='card'>
-                            
-                                <img src='$poster'>;
+                            $poster = $disk['poster'];
+                            $title = $disk['title'];
+                            $author = $disk['author'];
+                            $year = $disk['year'];
+                            echo  
+                            "<div class='card'>
+                                <img src='$poster'>
                                 <div class='title'>$title</div>
                                 <div class='author'>$author</div>
                                 <span class='year'>$year</span>
-                            
                             </div>";
                         }
                     ?>
